35263738ec350698319caac654316f1b94d9f22c
viren242 <virenmalavia242@gmail.com>
2023-02-05 19:04:33 -0400

    Pushing code

---
+++ src/pages/About.tsx
@@ -0,0 +1,11 @@
+export function About() {
+    return <>
+    <h1 className="display-5">About Us</h1>
+    <hr className="hr hr-blurry" />
+    <p>We are a leading online shopping destination, offering a wide range of products to customers worldwide. Our mission is to provide a seamless shopping experience, with easy navigation and a wide range of products to choose from. With a user-friendly interface, secure payment options, and fast delivery, we strive to make online shopping as convenient and hassle-free as possible.</p>
+
+    <p>Our team consists of experienced professionals who are dedicated to ensuring that our customers have a positive shopping experience. We believe in transparency and honesty, and always put our customers first. Our commitment to quality and customer satisfaction is evident in everything we do.</p>
+
+    <p>Thank you for choosing us as your online shopping partner. We look forward to serving you and making your shopping experience an enjoyable one.</p>
+    </>
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+export function Home() {
+    return <>
+    <h1 className="display-5">Home</h1>
+    <hr className="hr hr-blurry" />
+    <p><em><mark>Welcome to our online shopping site!</mark> We offer a wide selection of high-quality products, ranging from electronics and home goods to fashion and beauty items. With a user-friendly interface and secure payment options, shopping with us is easy and convenient.</em></p>
+
+    <p><em>Our mission is to provide a seamless shopping experience, with fast shipping and excellent customer service. Our knowledgeable and friendly customer support team is always here to help, and we're dedicated to ensuring that you're completely satisfied with your purchase.</em></p>
+
+    <p><em>Shop with confidence and enjoy the best online shopping experience at our site! Browse our products today and find everything you need to upgrade your life.</em></p>
+    </>
+}
